5f3424841ab8aabc68ed0e8293767429c2bdaf0e
BringSalavat <kaan062004+github@gmail.com>
2022-10-28 19:53:03 +0300

    added Tilemap

---
+++ Learning Experience/Assets/Scripts/SmoothCam.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCam : MonoBehaviour
+{
+    public GameObject target;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
